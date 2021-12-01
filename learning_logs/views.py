4c6765289e50453f69e8b9665d0cc1626995b68f
Mark Matlock <techmatlock@gmail.com>
2021-12-01 16:41:42 -0600

    Refactored code into check_topic_owner().

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -21,9 +21,7 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
-    # Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
 
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
@@ -74,8 +72,7 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
